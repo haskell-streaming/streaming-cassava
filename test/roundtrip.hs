c4c1721e7fb02b828b4eb1d401af407914528839
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-06-19 11:08:25 +1000

    Finish off test suite

--- test/roundtrip.hs
+++ test/roundtrip.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE DeriveGeneric, FlexibleContexts, MultiParamTypeClasses, RankNTypes,
+             ScopedTypeVariables #-}
+
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
 {- |
    Module      : Main
    Description : Round-trip property testing
@@ -17,19 +22,67 @@ import qualified Streaming.Prelude as S
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
+import Test.QuickCheck.Instances ()
 
-import Control.Monad.Except
-import Data.Functor.Identity (Identity, runIdentity)
+import           Control.Monad.Except
+import           Data.Text            (Text)
+import qualified Data.Vector          as V
+import           GHC.Generics         (Generic)
 
 --------------------------------------------------------------------------------
 
 main :: IO ()
-main = undefined
+main = hspec $ do
+  describe "Plain records" $ do
+    prop "Just data" (useType encodeDecode)
+    prop "With headers" (useType encodeDecodeHeader)
+  describe "Named records" $ do
+    prop "Default order" (useType encodeDecodeNamed)
+    prop "Reversed order" (useType encodeDecodeNamedReordered)
 
 encodeDecode :: (FromRecord a, ToRecord a, Eq a) => [a] -> Bool
-encodeDecode as = either (const False) (as==) . streamList
-                  . decode NoHeader . encode Nothing
-                  . S.each $ as
+encodeDecode = encodeDecodeWith (decode NoHeader . encode Nothing)
 
-streamList :: Stream (Of a) (Except e) () -> (Either e [a])
-streamList = runExcept . S.toList_
+encodeDecodeHeader :: (DefaultOrdered a, FromRecord a, ToRecord a, Eq a)
+                      => [a] -> Bool
+encodeDecodeHeader = encodeDecodeWith (decode HasHeader . encodeDefault)
+
+encodeDecodeNamed :: (DefaultOrdered a, FromNamedRecord a, ToNamedRecord a, Eq a)
+                     => [a] -> Bool
+encodeDecodeNamed = encodeDecodeWith (decodeByName . encodeByNameDefault)
+
+encodeDecodeNamedReordered :: forall a. (DefaultOrdered a, FromNamedRecord a, ToNamedRecord a, Eq a)
+                              => [a] -> Bool
+encodeDecodeNamedReordered = encodeDecodeWith (decodeByName . encodeByName hdr)
+  where
+    hdr = V.reverse (headerOrder (undefined :: a))
+
+encodeDecodeWith :: (Eq a)
+                    => (forall m r. (MonadError CsvParseException m) => Stream (Of a) m r -> Stream (Of a) m r)
+                    -> [a] -> Bool
+encodeDecodeWith f as = either (const False) (as==)
+                        . runExcept
+                        . S.toList_
+                        . f
+                        . S.each
+                        $ as
+
+useType :: ([Test] -> r) -> [Test] -> r
+useType = id
+
+data Test = Test
+  { columnA            :: !Int
+  , longer_column_name :: !Text
+  , mebbe              :: !(Maybe Double)
+  } deriving (Eq, Show, Read, Generic)
+
+-- DeriveAnyClass doesn't work with these types because of the Maybe
+
+instance FromRecord Test
+instance ToRecord Test
+instance DefaultOrdered Test
+instance FromNamedRecord Test
+instance ToNamedRecord Test
+
+instance Arbitrary Test where
+  arbitrary = Test <$> arbitrary <*> arbitrary <*> arbitrary
